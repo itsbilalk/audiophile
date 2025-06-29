5db5f29b3677c2e2e9945ce9c4d377fb765e6b78
Blal <blalkhan94@gmail.com>
2025-06-29 22:48:00 +0100

    feature: header component for all device types implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/Header/Header";
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen md:w-full ">
+      <div className="min-h-screen md:w-full text-[var(--colour-white)]">
         <Header />
       </div>
     </>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,21 +6,21 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className=" flex bg-contain bg-no-repeat bg-[url(../../public/home/mobile/image-header.jpg)] md:bg-[url(../../public/home/tablet/image-header.jpg)]  lg:bg-[url(../../public/home/desktop/image-hero.jpg)] min-h-[750px] md:min-h-[1000px] lg:bg-[#191919] lg:bg-center">
-      <div className="flex justify-between text-[var(--colour-white)] w-full pt-5 pl-8 pr-8 pb-4 border-b-1  border-b-[var(--colour-grey)] h-fit md:pl-0 md:pr-0 md:ml-8 md:mr-8 lg:ml-70 lg:mr-70">
+    <div className=" flex bg-contain bg-no-repeat bg-[url(../../public/home/mobile/image-header.jpg)] md:bg-[url(../../public/home/tablet/image-header.jpg)]  xl:bg-[url(../../public/home/desktop/image-hero.jpg)] min-h-[550px]  lg:bg-center">
+      <div className="flex justify-between text-[var(--colour-white)] w-full pt-5 pl-8 pr-8 pb-4 border-b-1  border-b-[var(--colour-grey)] h-fit md:pl-0 md:pr-0 md:ml-8 md:mr-8 xl:ml-70 xl:mr-70">
         <Image
           src={menuIcon}
           alt="Open menu to see categories"
-          className="w-4 h-4 lg:hidden"
+          className="w-4 h-4 xl:hidden"
         />
         <Image
           src={logo}
           alt="Audiophile logo"
           width={143}
           height={25}
-          className="h-fit"
+          className="h-fit self-center"
         />
-        <div className="hidden lg:flex flex-row gap-10 tracking-[0.125em] text-[0.813rem]">
+        <div className="hidden xl:flex flex-row gap-10 tracking-[0.125em] text-[0.813rem]">
           <Link href="/" className="hover:text-[var(--colour-dark-orange)]">
             HOME
           </Link>
@@ -40,7 +40,7 @@ export default function Header() {
           alt="Open cart to see the items added to it"
           width={23}
           height={20}
-          className="h-fit"
+          className="h-fit self-center"
         />
       </div>
     </div>
