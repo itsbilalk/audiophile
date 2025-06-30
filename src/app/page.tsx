cb61f34d80719e245ca4f1a4f51bd0dbdbf5d62d
Blal <blalkhan94@gmail.com>
2025-06-30 23:51:29 +0100

    feature: implemented header information about the headphones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,24 @@ export default function Home() {
   return (
     <>
       <div className="min-h-screen md:w-full text-[var(--colour-white)]">
-        <Header />
+        <div className=" flex flex-col bg-contain bg-no-repeat bg-[url(../../public/home/mobile/image-header.jpg)] md:bg-[url(../../public/home/tablet/image-header.jpg)]  xl:bg-[url(../../public/home/desktop/image-hero.jpg)] min-h-[550px]  bg-center">
+          <Header />
+          <div className="flex flex-col w-94.5 justify-center items-center m-auto text-center xl:ml-[23%] ">
+            <p className="tracking-[0.625em] uppercase text-[#ffffff80] text-[0.875rem]">
+              new product
+            </p>
+            <h1 className="font-bold text-[2.25rem] tracking-[0.072em] leading-14.5 uppercase mt-4 md:text-[3.5rem] ">
+              xx99 mark II headphones
+            </h1>
+            <p className="mt-6 text-[0.938rem] text-center ml-6 mr-6 text-[#ffffff80] font-medium md:w-87">
+              Experience natural, lifelike audio and exceptional build quality
+              made for the passionate music enthusiast.
+            </p>
+            <button className="mt-7 uppercase w-40 h-10 bg-[var(--colour-dark-orange)] hover:bg-[var(--colour-light-orange)] cursor-pointer">
+              see product
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,43 +6,41 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className=" flex bg-contain bg-no-repeat bg-[url(../../public/home/mobile/image-header.jpg)] md:bg-[url(../../public/home/tablet/image-header.jpg)]  xl:bg-[url(../../public/home/desktop/image-hero.jpg)] min-h-[550px]  lg:bg-center">
-      <div className="flex justify-between text-[var(--colour-white)] w-full pt-5 pl-8 pr-8 pb-4 border-b-1  border-b-[var(--colour-grey)] h-fit md:pl-0 md:pr-0 md:ml-8 md:mr-8 xl:ml-70 xl:mr-70">
-        <Image
-          src={menuIcon}
-          alt="Open menu to see categories"
-          className="w-4 h-4 xl:hidden"
-        />
-        <Image
-          src={logo}
-          alt="Audiophile logo"
-          width={143}
-          height={25}
-          className="h-fit self-center"
-        />
-        <div className="hidden xl:flex flex-row gap-10 tracking-[0.125em] text-[0.813rem]">
-          <Link href="/" className="hover:text-[var(--colour-dark-orange)]">
-            HOME
-          </Link>
-          <Link href="/" className="hover:text-[var(--colour-dark-orange)]">
-            HEADPHONES
-          </Link>
-          <Link href="/" className="hover:text-[var(--colour-dark-orange)]">
-            SPEAKERS
-          </Link>
-          <Link href="/" className="hover:text-[var(--colour-dark-orange)]">
-            EARPHONES
-          </Link>
-        </div>
-
-        <Image
-          src={cart}
-          alt="Open cart to see the items added to it"
-          width={23}
-          height={20}
-          className="h-fit self-center"
-        />
+    <div className="flex justify-between text-[var(--colour-white)] w-full pt-5 pl-8 pr-8 pb-4 border-b-1  border-b-[var(--colour-grey)] h-fit md:w-auto md:pl-0 md:pr-0 md:ml-8 md:mr-8 xl:ml-70 xl:mr-70 xl:w-auto">
+      <Image
+        src={menuIcon}
+        alt="Open menu to see categories"
+        className="w-4 h-4 xl:hidden self-center"
+      />
+      <Image
+        src={logo}
+        alt="Audiophile logo"
+        width={143}
+        height={25}
+        className="h-fit self-center md:mr-auto md:ml-10.5 xl:ml-0"
+      />
+      <div className="hidden xl:flex flex-row gap-10 tracking-[0.125em] text-[0.813rem] xl:mr-auto">
+        <Link href="/" className="hover:text-[var(--colour-dark-orange)]">
+          HOME
+        </Link>
+        <Link href="/" className="hover:text-[var(--colour-dark-orange)]">
+          HEADPHONES
+        </Link>
+        <Link href="/" className="hover:text-[var(--colour-dark-orange)]">
+          SPEAKERS
+        </Link>
+        <Link href="/" className="hover:text-[var(--colour-dark-orange)]">
+          EARPHONES
+        </Link>
       </div>
+
+      <Image
+        src={cart}
+        alt="Open cart to see the items added to it"
+        width={23}
+        height={20}
+        className="h-fit self-center"
+      />
     </div>
   );
 }
